6259b52cd1fa0c3cc65e134e444933cb5880e8af
eugenia0804 <67468941+eugenia0804@users.noreply.github.com>
2024-02-12 21:19:27 -0800

    completed outline & layout

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
-const shape = document.getElementById('shape');
-let currentShapeIndex = 0;
-const shapes = ['circle', 'square'];
+window.addEventListener('scroll', function() {
+  var scrollPosition = window.scrollY;
+  var image1 = document.getElementById('sidebarImage1');
+  var image2 = document.getElementById('sidebarImage2');
+  var containerHeight = document.querySelector('.container').clientHeight;
 
-document.getElementById('red-slider').addEventListener('input', updateColor);
-document.getElementById('green-slider').addEventListener('input', updateColor);
-document.getElementById('blue-slider').addEventListener('input', updateColor);
-
-function prevShape() {
-  currentShapeIndex = (currentShapeIndex - 1 + shapes.length) % shapes.length;
-  updateShape();
-}
-
-function nextShape() {
-  currentShapeIndex = (currentShapeIndex + 1) % shapes.length;
-  updateShape();
-}
-
-function updateShape() {
-  const selectedShape = shapes[currentShapeIndex];
-  shape.style.borderRadius = selectedShape === 'circle' ? '50%' : '0';
-}
-
-function updateColor() {
-  const red = document.getElementById('red-slider').value;
-  const green = document.getElementById('green-slider').value;
-  const blue = document.getElementById('blue-slider').value;
-
-  shape.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-}
+  if (scrollPosition > containerHeight) {
+      image1.style.filter = "grayscale(100%)";
+      image2.style.filter = "grayscale(100%)";
+  } else {
+      image1.style.filter = "none";
+      image2.style.filter = "none";
+  }
+});
